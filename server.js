9c558885d084c7f6dc11cb96ba125f2dcb5a4007
Felipedjsdv <felipeliveiptv@gmail.com>
2025-07-22 00:32:46 -0300

    Upload final

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/', userRoutes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log('Servidor rodando na porta ' + PORT);
 });
