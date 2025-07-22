94e66ce746f377aa3cd480a3c0fa516355ce4211
Felipedjsdv <felipeliveiptv@gmail.com>
2025-07-22 00:16:41 -0300

    server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
-
 const app = express();
+const userRoutes = require('./userRoutes');
+require('./db');
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use('/', userRoutes);
 
-app.use('/api', userRoutes);
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log('Servidor rodando na porta ' + PORT);
 });
