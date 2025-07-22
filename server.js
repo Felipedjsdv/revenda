2e0b84d4087ab2507f4cd1ec67cde52dd830187e
Felipedjsdv <felipeliveiptv@gmail.com>
2025-07-22 00:40:42 -0300

    Commit final

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const userRoutes = require('./userRoutes');
+require('./db');
+
+app.use(cors());
+app.use(express.json());
+app.use('/', userRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' + PORT);
+});
